870367db53e518c5fe0ba700f20c03ae7c7e79ff
H1cham1 <143965093+H1cham1@users.noreply.github.com>
2023-09-25 12:14:11 +0200

    Update simplepalindromechecker.py

--- Arch 1/Week 3/simplepalindromechecker.py
+++ Arch 1/Week 3/simplepalindromechecker.py
@@ -1,8 +1,14 @@
 Text = input("Please enter a text: ").lower()
 
-for char in Text:
-    if Text == Text[::-1]: print('"'+ Text +'"', 'is a palindrome')
-    elif Text != Text[::-1]:
-        print('"'+ Text +'"', 'is not a palindrome')
-        break
+index = len(Text) - 1
+
+Reversed_Text = ""
+
+while index >= 0:
+    Reversed_Text += Text[index]
+    index -= 1
+
+if Reversed_Text == Text: 
+    print('"' + Text + '"', 'is a palindrome')
+
 else: print('"'+ Text +'"','is not a palindrome')
